886ec7b1729f172b8ab1146130bdda6ce8007dcb
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:18:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "terptrek_29524Identifier",
+  url: "https://terptrek_29524Identifier.botics.co",
+  api: "https://terptrek_29524Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
